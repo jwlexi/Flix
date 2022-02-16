b0bf83ebc7453f8fd23e4b2189cdc8619eff9f08
Alexis Sanchez <53790532+jwlexi@users.noreply.github.com>
2022-02-15 19:26:09 -0800

    Flix part 2 requirements
    Finishing up the part two requirements

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Alexis Sanchez on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
